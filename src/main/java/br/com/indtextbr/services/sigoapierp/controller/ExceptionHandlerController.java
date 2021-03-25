195a7697da8a66bbfbc3d93850b0b5490e1b0dde
Goncalves <bags@gft.com>
2021-03-25 07:50:03 -0300

    Mudança no POM para carregar dependencia de teste

--- src/main/java/br/com/indtextbr/services/sigoapierp/controller/ExceptionHandlerController.java
+++ src/main/java/br/com/indtextbr/services/sigoapierp/controller/ExceptionHandlerController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.requestreply.KafkaReplyTimeoutException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpStatusCodeException;
